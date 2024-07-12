54f34226ae88e0d1d2df6e738c883ea8b22bb08b
cyp <cypriane81@gmail.com>
2024-07-12 20:51:11 +0100

    [+Added] infinity and beyond

--- Scripts/Game/Background.gd
+++ Scripts/Game/Background.gd
@@ -24,15 +24,49 @@ func _ready():
 func _process(_dt):
 	while cam.position.y < currentCentre.y - (textureSize / 2):
 		MoveUp()
+	while cam.position.y > currentCentre.y + (textureSize / 2):
+		MoveDown()
+	while cam.position.x < currentCentre.x - (textureSize / 2):
+		MoveLeft()
+	while cam.position.x > currentCentre.x + (textureSize / 2):
+		MoveRight()
 
-func MoveUp():
-	currentCentre.y -= textureSize
-	stars[2].position.y -= textureSize * 2
-	stars[3].position.y -= textureSize * 2
-
+func FlipV():
 	var newStars: Array[Sprite2D] = []
 	newStars.append(stars[2])
 	newStars.append(stars[3])
 	newStars.append(stars[0])
 	newStars.append(stars[1])
 	stars = newStars
+
+func FlipH():
+	var newStars: Array[Sprite2D] = []
+	newStars.append(stars[1])
+	newStars.append(stars[0])
+	newStars.append(stars[3])
+	newStars.append(stars[2])
+	stars = newStars
+
+func MoveUp():
+	currentCentre.y -= textureSize
+	stars[2].position.y -= textureSize * 2
+	stars[3].position.y -= textureSize * 2
+	FlipV()
+
+func MoveDown():
+	currentCentre.y += textureSize
+	stars[0].position.y += textureSize * 2
+	stars[1].position.y += textureSize * 2
+	FlipV()
+
+func MoveLeft():
+	currentCentre.x -= textureSize
+	stars[1].position.x -= textureSize * 2
+	stars[3].position.x -= textureSize * 2
+	FlipH()
+
+func MoveRight():
+	currentCentre.x += textureSize
+	stars[0].position.x += textureSize * 2
+	stars[2].position.x += textureSize * 2
+	FlipH()
